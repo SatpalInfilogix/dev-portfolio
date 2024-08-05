eca26fe2e09ee5e866ee0570c0b9344579f99dbf
SatpalInfilogix <satpalinfilogix@gmail.com>
2024-08-05 13:34:33 +0530

    Initialized basic controllers and admin dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', );
+    Route::get('/', [DashboardController::class, 'redirectToDashboard']);
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
 
